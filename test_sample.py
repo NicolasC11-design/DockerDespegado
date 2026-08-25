fd3aed132318ac3a21eec9b46bad4391e8bbfabb
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2026-08-25 11:18:13 -0500

    actividad test

--- test_sample.py
+++ test_sample.py
@@ -4,15 +4,15 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
 
-from backend.sample_app import app  
+from sample_app import sample
 
 def test_ejemploBasico():
     assert 1 + 1 == 2
 
 @pytest.fixture
 def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
+    sample.config['TESTING'] = True
+    with sample.test_client() as client:
         yield client
 
 def test_home_route_returns_200(client):
