553d3d48ea2ab77e21ed6725689975d9d18a9a2d
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2026-08-25 11:07:27 -0500

    Test actividad

--- test_sample.py
+++ test_sample.py
@@ -1,2 +1,15 @@
+import pytest
+from backend.sample_app import app  # Importa la instancia de tu aplicación Flask
+
 def test_ejemploBasico():
     assert 1 + 1 == 2
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_home_route_returns_200(client):
+    response = client.get('/')
+    assert response.status_code == 200
