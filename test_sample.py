445a80816eda55a34fff5f9158b65e3ba8da464d
NicolasC11-design <jostynnicolascristianobeltran@gmail.com>
2026-08-25 11:14:33 -0500

    Test actividad fix

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,10 @@
 import pytest
-from backend.sample_app import app  # Importa la instancia de tu aplicación Flask
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'backend')))
+
+from backend.sample_app import app  
 
 def test_ejemploBasico():
     assert 1 + 1 == 2
